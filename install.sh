febeb4b19db5678b913cd95b30528441d6df509f
wkt <weikting@gmail.com>
2023-11-09 14:17:00 +0800

    改进 离线包的安装脚本

--- install.sh
+++ install.sh
@@ -2,8 +2,24 @@
 
 SELF=$(readlink -f $0)
 SELF_DIR=$(dirname ${SELF})
-PACK_LIST=/etc/apt/sources.list.d/pack.list
+
+TM=$(date '+%s')
+
+PACK_LIST=/etc/apt/sources.list.d/pack_${TM}.list
+LIST_F=/etc/apt/sources.list
+LIST_B=/etc/apt/sources.list.${TM}
+
 cd ${SELF_DIR}
+
+uid=$(id -u)
+test "x${uid}" != "x0" && {
+	echo ""
+	echo "    Please run as this script as root"
+	echo ""
+	exit 1
+}
+
+
 if test -f ${SELF_DIR}/Packages.gz ;then
 cat <<__EOF > ${PACK_LIST}
 deb [allow-insecure=true trusted=true] file://${SELF_DIR} ./
@@ -12,8 +28,10 @@ __EOF
 fi
 
 rm -rf /var/lib/apt/lists/*
-mv /etc/apt/sources.list{,.bak}
+
+mv ${LIST_F} ${LIST_B}
 apt-get update
-mv /etc/apt/sources.list{.bak,}
+mv ${LIST_B} ${LIST_F}
+
 apt-get install --yes @PACKAGE_NAMES@
 rm -rf ${PACK_LIST}
